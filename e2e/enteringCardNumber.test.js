00802b15ac60647a3a96954307f8f9ec52afcdf4
vadim010975 <vadim010975@mail.ru>
2023-12-03 12:54:09 +0300

    e2e fixed

--- e2e/enteringCardNumber.test.js
+++ e2e/enteringCardNumber.test.js
@@ -21,7 +21,7 @@ describe("enteringCardNumber", () => {
     });
 
     browser = await puppeteer.launch({
-      headless: false,
+      headless: "new",
       slowMo: 100,
       // devtools: true,
     });
